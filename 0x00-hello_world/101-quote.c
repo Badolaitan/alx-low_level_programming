c1a54c73192605c9b2539373c451c993d5bb0042
olakitan <olaitanbadmus20@gmail.com>
2022-09-14 11:05:29 -0700

    _putchar commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Description:main- the main entry point
+ *
+ *
+ * Return: will be returning 1 (success) without warning
+ */
+
+int main(void)
+{
+	write(STDOUT_FILENO,stderr,"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
+	return (1)
+}
+	
